6697160d210d43146e6e02427de3bdcbb48291fe
Timothy Haynes <tim15811@gmail.com>
2016-07-30 13:52:39 -0400

    intial commit

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+Include this in gulpfile.js 'use strict';
+var gulp = require('gulp');
+var Sass = require('gulp-sass');
+gulp.task('Sass', function() {
+    return gulp.src('./styles/**/*.scss')
+        .pipe(Sass().on('error', Sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+gulp.task('Sass:watch', function() {
+    gulp.watch('./styles/**/*.scss', ['Sass']);
+});
